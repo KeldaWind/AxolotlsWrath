8e29bff015b15da40b14390c1f94f9fa47ec8c29
KeldaWind <sebastien.butor@orange.fr>
2018-11-09 17:41:41 +0100

    Difficulty Values

--- Assets/GRP23/Scripts/General/GameManager.cs
+++ Assets/GRP23/Scripts/General/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace GRP23
 {
     namespace AxolotlWrath
-    {
+    {       
         public class GameManager : MonoBehaviour
         {
             public static GameManager gameManager;
@@ -19,10 +19,23 @@ namespace GRP23
                 }
             }
 
+            [SerializeField] IntDifficultyValues ints;
+            [SerializeField] TestDifficultyValues tests;
+
+
             private void Awake()
             {
                 gameManager = this;
             }
+        }
+
+        [System.Serializable]
+        public struct TestStruct
+        {
+            public int Int;
+            public float Float;
+            public bool Bool;
+            public Vector3 Vector;
         }
     }
 }
---
+++ Assets/GRP23/Scripts/General/GenericDifficultyValues.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace GRP23
+{
+    namespace AxolotlWrath
+    {
+        [System.Serializable]
+        public class GenericDifficultyValues<T>
+        {
+            public T easyValue;
+            public T normalValue;
+            public T hardValue;
+        }
+
+        [System.Serializable]
+        public class IntDifficultyValues : GenericDifficultyValues<int>
+        { }
+
+        [System.Serializable]
+        public class FloatDifficultyValues : GenericDifficultyValues<float>
+        { }
+
+        [System.Serializable]
+        public class BoolDifficultyValues : GenericDifficultyValues<bool>
+        { }
+
+        [System.Serializable]
+        public class Vector3DifficultyValues : GenericDifficultyValues<Vector3>
+        { }
+
+        [System.Serializable]
+        public class TestDifficultyValues : GenericDifficultyValues<TestStruct>
+        { }
+    }
+}
